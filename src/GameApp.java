6b53fdee8023b545d636472f5e9d1b4b982bfef7
정가현 <kahyun0608@gmail.com>
2024-10-24 22:07:40 +0900

    docs : #10

--- src/GameApp.java
+++ src/GameApp.java
@@ -7,7 +7,7 @@ public class GameApp {
     public static boolean start() throws BadException {
         Scanner sc = new Scanner(System.in);
         System.out.println();
-        System.out.println("환영합니다! 원하시는 번호를 입력해주세요.");
+        System.out.println("☆환영합니다! 원하시는 번호를 입력해주세요.☆");
         System.out.println("1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기");
 
         String start = sc.nextLine();
